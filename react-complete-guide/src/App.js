9aff7dbeed9160b1a2c26730f7e49bf70f241500
Tony Li <tonyli@tonys-mbp.home>
2021-03-24 00:13:56 +1100

    version2

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,11 +4,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Tony", age: "24"},
+      {name: "Max", age: "29"}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log("clicked");
+    //this.state.persons[0].name = "Alex"
+    this.setState({
+      persons: [
+        {name: "Tony", age: "24"},
+        {name: "Max", age: "49"}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, i am a react app</h1>
-        <Person></Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: racing</Person>
       </div>
       
     );
