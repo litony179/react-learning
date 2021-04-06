a431800567c85e949ac619b8266f33e658ff8d86
Tony Li <tonyli@Tonys-MacBook-Pro.local>
2021-04-07 09:01:35 +1000

    update

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,35 +1,120 @@
 import React, { Component } from 'react';
+import Styled from 'styled-components';
 
 import './App.css';
 import Person from './Person/Person';
 
+// const StyledButton = Styled.button`
+//   background-color: ${props => props.alt ? 'red' : 'green'};
+//   color: white;
+//   font: inherit;
+//   boder: 1px solid blue;
+//   padding: 8px;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+//     color: black;
+//   }
+// `;
+
+
 class App extends Component {
   state = {
     persons: [
-      {name: "Tony", age: "24"},
-      {name: "Max", age: "29"}
-    ]
+      {id: "asdf", name: "Tony", age: "24"},
+      {id: "safsf", name: "Max", age: "29"}
+    ],
+    showPersons: false
   }
 
-  switchNameHandler = () => {
-    //console.log("clicked");
-    //this.state.persons[0].name = "Alex"
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person
     this.setState({
-      persons: [
-        {name: "Tony", age: "24"},
-        {name: "Max", age: "49"}
-      ]
+      persons: persons
+    });
+  }
+
+  deletePersonsHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = (event) => {
+    const show = this.state.showPersons;
+    this.setState({
+      showPersons: !show
     })
   }
 
+
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      boder: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    };
+
+    let persons = null;
+
+    if(this.state.showPersons === true){
+      persons = (
+        <div>
+          {this.state.persons.map(
+            (person, index) => {
+              return <Person 
+                click={() => this.deletePersonsHandler(index)} 
+                name={person.name} 
+                age={person.age} 
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
+            }
+          )}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        
+          backgroundColor: 'salmon',
+          color: 'black'
+        
+      }
+    }
+
+    const classes = [];
+    if(this.state.persons.length < 2){
+      classes.push('red');
+    }
+
+    if(this.state.persons.length < 1){
+      classes.push('bold');
+    }
+
     return (
-      <div className="App">
-        <h1>Hi, i am a react app</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: racing</Person>
-      </div>
+     
+        <div className="App">
+          <h1>Hi, i am a react app</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle persons</StyledButton>
+          {persons}
+        </div>
+     
       
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,13 +1,32 @@
 import React from 'react';
+//import './Person.css';
+import styled from 'styled-components';
 
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: auto;
+    border: 1px solid #eee;
+    box-shadow: 1px 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    margin-bottom: 16px;
+
+    @media (min-width: 500px) {
+        width: 450px;
+    }
+`;
 
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old</p>
+        // <div className="Person" style={style}>
+        <StyledDiv>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
-        </div>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </StyledDiv>
+            
         
     )
 };
